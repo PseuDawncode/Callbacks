4ec82131aed92d662b8d8e645944b89f20ac59f3
mdmalida <dawnmarymalida@gmail.com>
2024-10-01 19:28:21 +0200

    first commit

---
+++ aboutMe.js
@@ -0,0 +1,21 @@
+const fetchData = () => {
+    console.log ("My name is Mary Dawn Malida, please call me Dawn for short. :)");
+    setTimeout(() =>{
+        console.log ("(2 seconds delay)");
+        console.log ("I am 27 years old");
+    },2000)
+
+    setTimeout(() =>{
+        console.log ("(3 seconds delay)");
+        console.log ("I was born in the Southern Philippines but now living in Landskrona, Sweden.");
+    },3000)
+
+    setTimeout(() =>{
+        console.log ("(4 seconds delay)");
+        console.log ("I love travelling, cooking, cycling, and dancing.");
+    },4000)
+
+
+};
+
+fetchData();
